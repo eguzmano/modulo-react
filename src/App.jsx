14b4bde91d598b18dc3f9489ee2bb2e986dea60f
Esteban Guzman <guzmanosorioesteban@gmail.com>
2025-02-16 16:19:59 -0300

    comment components

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
 import './App.css'
 import Cart from './components/Cart'
 import Footer from './components/Footer'
-import Home from './components/Home'
+// import Home from './components/Home'
 import Navbar from './components/Navbar'
-import Register from './components/Register'
-import Login from './components/Login'
+// import Register from './components/Register'
+// import Login from './components/Login'
 
 const App = () => {
   return (
     <div className='wrapper'>
       <Navbar />
       <div className='content'>
-        <Home />
-        <Register />
-        <Login />
+        {/* <Home /> */}
+        {/* <Register /> */}
+        {/* <Login /> */}
         <Cart />
       </div>
       <Footer />
